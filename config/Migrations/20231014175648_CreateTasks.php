139fad0f7574d5a5d49b964ba1e6d9b02abcfa24
Oriana Morillo <orianacmv@gmail.com>
2023-10-14 15:02:30 -0300

    add migrations

---
+++ config/Migrations/20231014175648_CreateTasks.php
@@ -0,0 +1,38 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateTasks extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('tasks');
+        $table->addColumn('description', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('status_id', 'integer', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addForeignKey('status_id', 'statuses', 'id', [
+            'delete' => 'SET_NULL',
+            'update' => 'NO_ACTION'
+        ]);
+        $table->create();
+    }
+}

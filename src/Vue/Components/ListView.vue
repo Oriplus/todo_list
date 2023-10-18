1163b7467d52e65953f0b320a33d1189b1f54292
Oriana Morillo <orianacmv@gmail.com>
2023-10-18 01:18:06 -0300

    add component

---
+++ src/Vue/Components/ListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="taskItem in tasks" :key="taskItem.id">
+      <ListTask :taskItem="taskItem" v-on:taskChanged="$emit('reloadTasks')"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListTask from './ListTask.vue';
+
+
+export default {
+  props: ['tasks'],
+  components: { ListTask }
+}
+</script>
